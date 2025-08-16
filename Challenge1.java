0240a0d1e95167abdefbf64e815ca3fe84aec4c2
Tejaspatil2424 <tejaspatil002424@gmail.com>
2025-08-16 15:56:35 +0530

    Problem 1 solved on 16/08/2025.

--- Challenge1.java
+++ Challenge1.java
@@ -2,7 +2,7 @@ public class Challenge1 {
 
     public static void main(String[] args) {
         
-        int [] arr={1,2,4,5,6,7,8,9};
+        int [] arr={1,2,4,5};
 
         int sum1=0;
 
@@ -19,4 +19,5 @@ public class Challenge1 {
 
         System.out.println("Missing value is:"+ (sum2-sum1));
     }
+
 }
